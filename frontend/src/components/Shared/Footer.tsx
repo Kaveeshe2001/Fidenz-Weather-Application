6c6e925d20b17f9bf0cb44a9ac1eefbed57cc125
Kaveesha <kaveeshewaduge111@gmail.com>
2025-10-16 00:11:33 +0530

    Footer

---
+++ frontend/src/components/Shared/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full bg-[rgba(var(--primary-color3-opc),0.06)] text-gray-500 bg">
+        <p className="p-5 text-center text-xs md:text-sm">
+            Copyright 2025 © Forecast.io. All Right Reserved.
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import Footer from "../../components/Shared/Footer"
 import Navbar from "../../components/Shared/Navbar"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   )
 }
