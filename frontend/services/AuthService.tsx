5c111e3ab399371962763b98ee8248704f57a04e
Kaveesha <kaveeshewaduge111@gmail.com>
2025-10-15 09:19:17 +0530

    AuthServices

--- frontend/services/AuthService.tsx
+++ frontend/services/AuthService.tsx
@@ -1,2 +1,30 @@
-const api = 'http://localhost:5257/server_saas/';
+import axios from "axios";
+import { AuthResponse, LoginRequest } from "../models/User";
+import { handleError } from "../handlers/ErrorHandler";
 
+const api = 'http://localhost:5257/backend/';
+
+export const loginAPI = async (credentials: LoginRequest) => {
+    try {
+        const response = await axios.post<AuthResponse>(api + 'auth/login', {
+            credentials
+        });
+
+        if (response.data.access_token) {
+            localStorage.setItem('accessToken', response.data.access_token);
+        }
+
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const logout = (): void => {
+    localStorage.removeItem('accessToken');
+    window.location.href = '/login';
+};
+
+export const getToken = (): string | null => {
+    return localStorage.getItem('accessToken');
+}
