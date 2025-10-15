65e03181318ad5cbce2601ed684b5cc0bcff4f97
Kaveesha <kaveeshewaduge111@gmail.com>
2025-10-15 20:17:38 +0530

    WeatherController

---
+++ backend/Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+﻿using backend.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("backend/weather")]
+    [ApiController]
+    [Authorize]
+    public class WeatherController : Controller
+    {
+        private readonly IWeatherService _weatherService;
+
+        public WeatherController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeatherData()
+        {
+            var weatherData = await _weatherService.GetWeatherForAllCitiesAsync();
+            return Ok(weatherData);
+        }
+    }
+}
