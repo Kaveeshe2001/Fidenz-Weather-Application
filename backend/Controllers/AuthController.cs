0ddb942fa3911de91fbe0dbc2b2239758ebe7bf8
Kaveesha <kaveeshewaduge111@gmail.com>
2025-10-16 18:13:08 +0530

    Update Authcontroller for handle MFA

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -35,9 +35,42 @@ namespace backend.Controllers
 
             RestResponse response = await client.ExecuteAsync(request);
 
+            if (!response.IsSuccessful && response.Content.Contains("mfa_required"))
+            {
+                var mfaResponse = JsonSerializer.Deserialize<MfaRequiredResponseDto>(response.Content);
+                //give the mfa token to frontend
+                return Ok(new { mfaRequired = true, mfaToken = mfaResponse.mfa_token });
+            }
+
             if (response.IsSuccessStatusCode)
             {
-                var tokenData = JsonSerializer.Deserialize<AuthResponseDto>(response.Content!);
+                var tokenData = JsonSerializer.Deserialize<AuthResponseDto>(response.Content);
+                return Ok(new { mfaRequired = false, token = tokenData });
+            }
+
+            return Unauthorized(response.Content);
+        }
+
+        [HttpPost("verify-mfa")]
+        // OTP verification
+        public async Task<IActionResult> VerifyMfa([FromBody] VerifyMfaRequestDto mfaRequest)
+        {
+            var client = new RestClient($"https://{_config["Auth0:Domain"]}");
+            var request = new RestRequest("/oauth/token", Method.Post);
+
+            request.AddJsonBody(new
+            {
+                grant_type = "http://auth0.com/oauth/grant-type/mfa-otp",
+                mfa_token = mfaRequest.MfaToken,
+                otp = mfaRequest.Otp,
+                client_id = _config["Auth0:ClientId"]
+            });
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                var tokenData = JsonSerializer.Deserialize<AuthResponseDto>(response.Content);
                 return Ok(tokenData);
             }
 
---
+++ backend/DTO/AuthDtos/AuthDto.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.DTO.AuthDtos
+{
+    public class MfaRequiredResponseDto
+    {
+        public string mfa_token { get; set; }
+    }
+
+    public class VerifyMfaRequestDto
+    {
+        public string MfaToken { get; set; }
+        public string Otp { get; set; }
+    }
+}
